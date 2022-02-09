10ec5fc293d14f9a52e55d924a8766a0797ca120
Catharina Leth Andersen <catharinaleth@ruc-vpn-28-012-024.ruc.dk>
2022-02-09 11:11:16 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] minListe = new int [5];
+                minListe[0] = -88;
+                minListe[1] = 2;
+                minListe[2] = 20;
+                minListe[3] = 99;
+                minListe[4] = -40;
+
+        for (int i = 4; i >=0; i = i-1) {
+            System.out.println(minListe[i]);
+
+        }        
+
+    }
+}
